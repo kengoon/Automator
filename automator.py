3791ec5d2f5552a730990cd002915d9e0034da2c
Davide <80689057+Davide255@users.noreply.github.com>
2021-10-04 15:46:18 +0200

    Delete automator.py

--- automator.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-if len(sys.argv) != 1:
-    print(sys.argv)
-else:
-    os.chdir('D:\desktop\python\Automator\Automator\data')
-    os.startfile('D:\desktop\python\Automator\Automator\data\main.py')
